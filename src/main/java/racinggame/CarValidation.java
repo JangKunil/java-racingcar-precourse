27ae87a84ea0e5de47dbae3720219d465c4cd718
JangKunil <jangkeya@gmail.com>
2021-10-12 18:25:46 +0900

    [feat] : 차 이름 관련, 예외처리 3가지 추가  - 빈 이름이 들어올 수 없다  - 1 ~ 5글자 사이의 이름만 허용된다  - 중복된 차 이름이 존재할 수 없다

---
+++ src/main/java/racinggame/CarValidation.java
@@ -0,0 +1,40 @@
+package racinggame;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CarValidation {
+    public static boolean carNameValidation(String[] cars) {
+        return isNotEmptyName(cars) && isRightLength(cars) && isNonDuplicate(cars);
+    }
+
+    private static boolean isNotEmptyName(String[] cars) {
+        for (String carName : cars) {
+            if (carName.equals(Constant.EMPTY_NAME)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isRightLength(String[] cars) {
+        for (String carName : cars) {
+            if (carName.length() < Constant.MIN_CAR_NAME_LENGTH
+                    || carName.length() > Constant.MAX_CAR_NAME_LENGTH) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isNonDuplicate(String[] cars) {
+        Set<String> carList = new HashSet<>();
+
+        for (String carName : cars) {
+            if (!carList.add(carName)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
--- src/main/java/racinggame/Constant.java
+++ src/main/java/racinggame/Constant.java
@@ -2,4 +2,13 @@ package racinggame;
 
 public class Constant {
     public static final String PARTICIPATING_CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    public static final String ERROR_PREFIX = "[ERROR] ";
+    public static final String ERROR_CAR_NAME = ERROR_PREFIX + "올바른 입력의 이름이 아닙니다.";
+
+    public static final String EMPTY_NAME = "";
+    public static final String DELIMITER = ",";
+
+    public static final int MIN_CAR_NAME_LENGTH = 1;
+    public static final int MAX_CAR_NAME_LENGTH = 5;
 }
